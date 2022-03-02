bceb03dfc6e0ad60966e6392260585dbad45a725
tanerdurmaz <taner.durmaz.cs@gmail.com>
2022-03-02 17:08:33 +0300

    Updates

--- buildUnity/Build/buildUnity.framework.js
+++ buildUnity/Build/buildUnity.framework.js
@@ -1292,7 +1292,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2479872;
+STATICTOP = STATIC_BASE + 2479344;
 __ATINIT__.push({
  func: (function() {
   ___cxx_global_var_init_8();
@@ -3362,7 +3362,7 @@ __ATINIT__.push({
   ___cxx_global_var_init_175();
  })
 });
-var STATIC_BUMP = 2479872;
+var STATIC_BUMP = 2479344;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -15287,8 +15287,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 56073;
-Module["wasmMaxTableSize"] = 56073;
+Module["wasmTableSize"] = 56069;
+Module["wasmMaxTableSize"] = 56069;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
